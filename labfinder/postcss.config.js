58fc143647688e0f5c46e12cf066492e28c6005b
kimjin373 <kimjin373@kentech.ac.kr>
2025-06-12 05:34:42 +0900

    chore: 불필요한 파일 정리 및 프로젝트 구조 개선

--- labfinder/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.cjs
-module.exports = {
-  plugins: {
-    // 기존 tailwindcss 대신, 분리된 패키지를 가리킵니다
-    "@tailwindcss/postcss": {},
-    // autoprefixer는 그대로
-    autoprefixer: {},
-  },
-}
